926313f8fd6e44c46cafd18996508487e2bbdfaf
Andrezinrc <andrem.33333@gmail.com>
2024-04-11 06:28:07 -0300

    criando o meu portfolio

---
+++ main.js
@@ -0,0 +1,8 @@
+function animar() {
+
+  var btn = document.getElementById("btn-menu");
+  btn.classList.toggle('ativar')
+
+  var menu = document.getElementById("menu")
+  menu.classList.toggle('abrirMenu');
+}
